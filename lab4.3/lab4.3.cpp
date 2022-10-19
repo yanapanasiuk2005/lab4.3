91bbc028c75f5ba3748ae91695aba39fff39121f
yanapanasiuk2005 <yana.panasiuk.ri.2022@lpnu.ua>
2022-10-19 22:59:19 +0300

    Add project files.

---
+++ lab4.3/lab4.3.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x, xp, xk, dx, a, b, c, F;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << "a = "; cin >> a;
+	cout << "b = "; cin >> b;
+	cout << "c = "; cin >> c;
+
+	cout << fixed;
+	cout << "---------------------------" << endl;
+	cout << "|" << setw(5) << "x" << " |"
+		<< setw(7) << "F" << " |" << endl;
+	cout << "---------------------------" << endl;
+	x = xp;
+	while (x <= xk)
+	{
+		if (x + 10 < 0 && b != 0)
+			F = a * (x * x) - c * x + b;
+		else
+			if (x + 10 > 0 && b == 0)
+				F = (x - a)/(x - c);
+			else
+				F = ((-1) * x)/(a - c);
+		cout << "|" << setw(7) << setprecision(2) << x
+			<< " |" << setw(10) << setprecision(3) << F
+			<< " |" << endl;
+		x += dx;
+	}
+	cout << "---------------------------" << endl;
+	return 0;
+}
